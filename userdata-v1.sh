a1b5037b19931c7925303d5b514978e8c8f132c1
Viet Tran <hello@viet.vn>
2026-06-19 22:38:14 +0700

    Viet Tran - hello@viet.vn

--- userdata-v1.sh
+++ userdata-v1.sh
@@ -8,3 +8,7 @@ dnf install httpd -y
 
 # Create a Simple static file on webserver
 echo "Hello World from AWS!" > /var/www/html/index.html
+
+# Start the httpd service
+systemctl start httpd
+systemctl enable httpd
