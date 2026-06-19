0aed5e680680dcfc796df49eee5b7432e1b505c3
Viet Tran <hello@viet.vn>
2026-06-19 22:32:55 +0700

    Viet Tran - hello@viet.vn

---
+++ userdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update system
+dnf update -y
+
+# Install simple http server to serve the React app
+dnf install httpd -y
+
+# Create a Simple static file on webserver
+echo "Hello World from AWS!" > /var/www/html/index.html
